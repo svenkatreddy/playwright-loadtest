e0ce09449ef4f837fc07dcc4788499b9ca9cace8
svenkatreddy <svenkatreddy@users.noreply.github.com>
2021-05-06 19:18:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# playwright-loadtest
+
+[![Build Status](https://travis-ci.org/svenkatreddy/playwright-loadtest.svg?branch=master)](https://travis-ci.org/svenkatreddy/playwright-loadtest)
+
+[![NPM](https://nodei.co/npm/playwright-loadtest.png?stars=true)](https://nodei.co/npm/playwright-loadtest/)
+
+playwright-loadtest provides a simple way to launch multiple playwright instances in parallel to run a simple load test on your site.
+
+## Installation
+
+Install via npm:
+
+    $ npm install -g playwright-loadtest
+
+## Usage
+
+To run a basic load test, just supply the name of a playwright script to run:
+
+    $ playwright-loadtest --file=sample.js
+
+This will run the specified playwright script once in chrome headless instance.
+
+### Parameters
+
+`--s` flag is to mention sample size
+
+`--c` flag is to mention number of concurrent executions per sample
+
+`--silent` boolean to enable or disable logs
+
+`--outputFile` send performance results to output file
+
+    $ playwright-loadtest --s=100 --c=25 --file=sample.js
+    
+This will run a total of 100 runs through the specified playwright script across 25 concurrent chrome headless instances.
+
+
+### Examples
+
+    $ playwright-loadtest --file=sample.js
+    
+    $ playwright-loadtest --file=./test/sample.js  --s=100 --c=25
+    
+    $ playwright-loadtest --file=./test/sample.js  --s=100 --c=25 --silent=true
+    
+    $ playwright-loadtest --file=./test/sample.js  -s 100 -c 25
+
+    $ playwright-loadtest --file=./test/sample.js  -s 100 -c 25 --outputFile=performance.json
+
+
+### use as node module 
+
+    const startplaywrightLoadTest = require('playwright-loadtest');
+    const results = await startplaywrightLoadTest({
+        file, // path to file
+        samplesRequested, // number of samples requested
+        concurrencyRequested, // number of concurrency requested
+    });
+    console.log(results);
+    
+   
+## Feedback   
+
+please provide feedback or feature requests using issues link
+
---
+++ index.js
@@ -0,0 +1,109 @@
+'use strict';
+
+const createDebug = require('debug');
+const debug = createDebug('playwright-loadtest');
+const exec = require('child_process').exec;
+const perf = require('execution-time')();
+
+const defaultOptions = {
+  file: '',
+  samplesRequested: '',
+  concurrencyRequested: 1,
+  results: {},
+  samplesCount: 0,
+}
+
+debug('playwright-loadtest is loading...');
+
+
+const startSampleLogPerformance = (results, samplesCount) => {
+  perf.start(`sampleCall${samplesCount + 1}`);
+  results[`sample${samplesCount + 1}`] = {};
+};
+
+const stopSampleLogPerformance = (results, samplesCount) => {
+  results[`sample${samplesCount + 1}`].sample = perf.stop(`sampleCall${samplesCount + 1}`);
+};
+
+const startConcurrencyLogPerformance = (results, concurrencyCount, samplesCount) => {
+  perf.start(`sample${samplesCount + 1}concurrencyCount${concurrencyCount + 1}`);
+  results[`sample${samplesCount + 1}`].concurrency = {};
+}
+
+const stopConcurrencyLogPerformance = (results, concurrencyCount, samplesCount) => {
+  if(results[`sample${samplesCount + 1}`]) {
+    results[`sample${samplesCount + 1}`].concurrency[`${concurrencyCount + 1}`] = perf.stop((`sample${samplesCount + 1}concurrencyCount${concurrencyCount + 1}`));
+  }
+}
+
+
+const executeTheCommand = function({ cmd, concurrencyCount, samplesCount, results }) {
+  return new Promise((resolve, reject) => {
+    startConcurrencyLogPerformance(results, concurrencyCount, samplesCount);
+    exec(cmd, function(error, stdout, stderr) {
+      debug(`sample: ${samplesCount}, concurrent: ${concurrencyCount}`);
+      stopConcurrencyLogPerformance(results, concurrencyCount, samplesCount);
+      if(stderr) reject(stderr);
+      if(error) reject(error);
+      resolve(stdout);
+    });
+  });   
+};
+
+const doAnotherSample = async (options) => { 
+  let {
+    concurrencyRequested,
+    file,
+    samplesCount,
+    samplesRequested,
+    results,
+  } = options;
+
+  if(samplesCount < samplesRequested) {
+    startSampleLogPerformance(results, samplesCount);
+    await doConcurrency({ results,  samplesCount, concurrencyRequested, file });
+    stopSampleLogPerformance(results, samplesCount);
+    samplesCount += 1;
+    return doAnotherSample({
+      concurrencyRequested,
+      file,
+      samplesCount,
+      samplesRequested,
+      results,
+    });
+  }
+
+  return results;
+};
+
+const doConcurrency = async ({ results,  samplesCount, concurrencyRequested, file}) => {
+  const promisesArray = [];
+
+  for(let i=0; i < concurrencyRequested; i += 1) {
+    promisesArray.push(
+      executeTheCommand({ 
+        cmd: `node ${file}`,
+        concurrencyCount: i,
+        results,
+        samplesCount,
+      })
+    );
+  }
+
+  let values;
+  try {
+    perf.start('concurrencyCall');
+    values = await Promise.all(promisesArray);
+    debug(values);
+  } catch(error) {
+    debug(error);
+  }
+  return values;
+};
+
+function startplaywrightLoadTest(paramOptions) {
+  const options = Object.assign({}, defaultOptions, paramOptions);
+  return doAnotherSample(options);
+}
+
+module.exports = startplaywrightLoadTest;
